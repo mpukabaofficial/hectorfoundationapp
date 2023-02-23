9108f430277f7ac8b2606146a110a50548e0401f
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2023-02-23 18:14:42 -0500

    until education, have to fix labels

--- pages/application/education.js
+++ pages/application/education.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Application from "../application/index.js";
+import styles from "../../styles/education.module.css";
 
 const Education = () => {
   return (
@@ -7,6 +8,61 @@ const Education = () => {
       <div className="title">
         <h1>Education</h1>
       </div>
+      <form action="submit" method="post" id="profile">
+        <section>
+          <h2>Current School</h2>
+          <div className={styles.content}>
+            <div>
+              <label for="PGname">Name</label>
+              <input type="text" id="PGname" name="PGname" required />
+            </div>
+            <div>
+              <label for="PGlast">Current Grade</label>
+              <input type="text" id="PGlast" name="PGlast" required />
+            </div>
+            <div>
+              <label for="dob">Since</label>
+              <input type="date" id="last" name="last" required />
+            </div>
+            <div>
+              <label for="dob">Anticipated Graduation</label>
+              <input type="date" id="last" name="last" required />
+            </div>
+            <div></div>
+          </div>
+        </section>
+        <section>
+          <h2>Other School</h2>
+          <div className={styles.content}>
+            <div>
+              <label for="PGname">Name</label>
+              <input type="text" id="PGname" name="PGname" required />
+            </div>
+            <div>
+              <label for="PGlast">Current Grade</label>
+              <input type="text" id="PGlast" name="PGlast" required />
+            </div>
+            <div>
+              <label for="dob">Since</label>
+              <input type="date" id="last" name="last" required />
+            </div>
+            <div>
+              <label for="dob">Graduation</label>
+              <input type="date" id="last" name="last" required />
+            </div>
+            <div></div>
+          </div>
+        </section>
+        <section>
+          <h2>Grades</h2>
+          <div className={styles.content}>
+            <div>
+              <label for="avatar">Please upload your transcripts: </label>
+              <input type="file" id="avatar" name="avatar" accept="*" />
+            </div>
+          </div>
+        </section>
+      </form>
       <div>
         <button
           type="submit"
--- pages/application/family.js
+++ pages/application/family.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Application from "../application/index.js";
+import styles from "../../styles/family.module.css";
 
 const Family = () => {
   return (
@@ -7,6 +8,47 @@ const Family = () => {
       <div className="title">
         <h1>Family</h1>
       </div>
+      <form action="submit" method="post" id="profile">
+        <section>
+          <h2>Parent or Guardian</h2>
+          <div className={styles.content}>
+            <div>
+              <label for="PGname">Name</label>
+              <input type="text" id="PGname" name="PGname" required />
+            </div>
+            <div>
+              <label for="PGlast">Last Name</label>
+              <input type="text" id="PGlast" name="PGlast" required />
+            </div>
+            <div>
+              <label for="relation">Relationship</label>
+              <input type="text" id="relation" name="relation" required />
+            </div>
+            <div></div>
+          </div>
+        </section>
+        <section>
+          <h2>Contact Details</h2>
+          <div className={styles.content}>
+            <div>
+              <label for="phone">Phone Number</label>
+              <input type="tel" id="phone" name="phone" required />
+            </div>
+            <div>
+              <label for="last">Email</label>
+              <input type="email" id="last" name="last" />
+            </div>
+            <div>
+              <label for="othrphone">Other Phone Number</label>
+              <input type="tel" id="othrphone" name="othrphone" />
+            </div>
+            <div>
+              <label for="otherEmail">Other Email</label>
+              <input type="email" id="otherEmail" name="otherEmail" />
+            </div>
+          </div>
+        </section>
+      </form>
       <div>
         <button
           type="submit"
